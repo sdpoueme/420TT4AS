32bf1e9b6abe6c4666622a6f96d8058e6615a7ed
Serge Poueme <sergepoueme@gmail.com>
2025-03-24 14:54:13 -0400

    Update UserManagement.jsx

--- UserManagement.jsx
+++ UserManagement.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-function RegisterForm() {
+function UserManagementForm() {
   const [formData, setFormData] = useState({
     username: '',
     email: '',
