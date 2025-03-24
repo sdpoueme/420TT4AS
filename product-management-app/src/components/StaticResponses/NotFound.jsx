d820443a5d5961450d06ad105eb75f464d18be80
Serge Poueme <sergepoueme@gmail.com>
2025-03-24 18:08:47 -0400

    Create NotFound.jsx

---
+++ product-management-app/src/components/StaticResponses/NotFound.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+
+
+function NotFound()
+
+{
+    return(
+        <div>
+            <h1>404</h1>
+            <h2>Page not found</h2>
+        </div>
+    )
+
+}
+
+export default NotFound;
