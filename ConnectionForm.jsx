0ddd1e62abe1737eb95a4872c8708b55da5d6429
Serge Poueme <sergepoueme@gmail.com>
2025-03-24 16:35:41 -0400

    Update ConnectionForm.jsx

--- ConnectionForm.jsx
+++ ConnectionForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
 
-function LoginForm() {
+function ConnectionForm() {
   const [credentials, setCredentials] = useState({
     email: '',
     password: ''
